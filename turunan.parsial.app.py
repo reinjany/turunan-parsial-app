253d57ece217be43238124e3fe13f6888a1d467c
reinjany <anandasyifa653@gmail.com>
2025-05-18 12:17:00 +0700

    Create turunan.parsial.app.py

---
+++ turunan.parsial.app.py
@@ -0,0 +1,42 @@
+o-ur) 7) s
+(QB autosave @ or) EF] D~ C) F 1746951261675 Pertemua... - Saved V PP Search {on} = a x
+File Home Insert Draw Design Layout References Mailings Review View Help | comments | | 7 Editing ~) (agsnereiee
+libri Cay I Vile. Ee. EES Pind » ou
+|Calibri vit. A A Aare AD fae cae ey BEST a 5 oo
+5) — No Spacing ¢C v Ge Replace
+= — —= —|,= _ c Rep! i
+Paste BIUvéax x Ay 2- A> [BJS SE IEe oe Be Add-ins
+- — }S Select
+Clipboard fS Font 5 Paragraph 5 Styles 5 Editing Add-ins v
+L Beene PDQ S14 Se Ge FelBel G1 101 LE 121131 14115116 1971 181191201 2b1 221 231 241251 261271 291 291301 311321331 341351 B6iSABBrtededetedererede dere
+buat dua fe a
+* 4.turunan_parsial_app.py
+Sain kode bert dan simpan dengan nama turunan_parsial_opp-by
+oyten coy vee xX_vals = np. Linspace(x@ - 2, x@ + 2, 50)
+Nee rani y_vals = np.Linspace(y@ - 2, y@ + 2, 50)
+import sympy as sp X, Y = np.meshgrid(x_vals, y_vals)
+snport nampy 95 Z = sp. lambdify((x, y), f, ‘numpy')(X, Y
+dnport matplotLib.pyptot as ptt
+Z.tangent = float(f_val) + float(fx_val)x(X - x0) + float(fy_val)#(Y - ye)
+setstse yn fig = plt.figure(figsize=(10, 6))
+oA orar adhere noth Hi ego ed ax = fig-add_subplot (111, projection='3d")
+ax.plot_surface(X, Y, 2, alpha=0.7, cnap='viridis')
+Or our to ax.plot_surface(X, Y, Z_tangent, alpha=0.5, color="red’)
+tee spite, 0 ax.set_title("Permukaan f(x, y) dan bidang singgungnya")
+fy = speditt(t, y) ax.set_xlabel('x')
+stelatex(f"f(x, y) = (sp.tatex(f)}") CE)
+stlatex(\\trac((\\partiat DHC\\partial 0) = (sp. latent D0") ax.set_zlabel('2')
+stulatextt™\\trac((\\partiol (partial 92) = dsps latex 129") st.pyplot(fig)
+YO = stenumber=input("Nilas ye:", vatuen2.0) except Exception as e:
+st.error(f"Terjadi kesalahan: {e}")
+vat = fesune(tat 98, y: y0))
+eval = fessubst Cat 98, y2 y0))
+fyival = fycsubs(Ox 38, 92 0D)
+stowrtel"Gragien ob 848 (xy ye)", ACPA, fy 90000")
+» 2. requirements.txt :
+Vv
+Ella ini hari Hhrans van aihusihban
+Page 110f17 128words Di English (Indonesia) 9% Accessibility: Unavailable ‘DY Focus fe -—H ++ 45%
+3 = A > 12:12
+$3 1 G ma (ey w A @ mc Fo)
+* Q search cae) 9 a g @k DY © 18/05/2025
